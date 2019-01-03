b96a9cdd436dff0a65782a516f5ce2c73d419569
Jacob Beck <jake@fishtownanalytics.com>
2019-01-03 13:43:08 -0700

    Move SQL previously embedded into adapters into macros
    Adapters now store an internal manifest that only has the dbt internal projects
    Adapters use that manifest if none is provided to execute_manifest
    The internal manifest is lazy-loaded to avoid recursion issues
    Moved declared plugin paths down one level
    Connection management changes to accomadate calling macro -> adapter -> macro
    Split up precision and scale when describing number columns so agate doesn't eat commas
    Manifest building now happens in the RunManager instead of the compiler
    
    Now macros:
      create/drop schema
      get_columns_in_relation
      alter column type
      rename/drop/truncate
      list_schemas/check_schema_exists
      list_relations_without_caching

--- dbt/include/redshift/macros/adapters.sql
+++ dbt/include/redshift/macros/adapters.sql
@@ -64,3 +64,99 @@
   {{ dist('dbt_updated_at') }}
   {{ sort('compound', ['scd_id']) }};
 {%- endmacro %}
+
+
+{% macro redshift__create_schema(database_name, schema_name) -%}
+  {{ postgres__create_schema(database_name, schema_name) }}
+{% endmacro %}
+
+{% macro redshift__drop_schema(database_name, schema_name) -%}
+  {{ postgres__drop_schema(database_name, schema_name) }}
+{% endmacro %}
+
+
+{% macro redshift__get_columns_in_relation(relation) -%}
+  {% call statement('get_columns_in_relation', fetch_result=True) %}
+      with bound_views as (
+        select
+          ordinal_position,
+          table_schema,
+          column_name,
+          data_type,
+          character_maximum_length,
+          numeric_precision,
+          numeric_scale
+
+        from information_schema.columns
+        where table_name = '{{ relation.identifier }}'
+    ),
+
+    unbound_views as (
+      select
+        ordinal_position,
+        view_schema,
+        col_name,
+        case
+          when col_type ilike 'character varying%' then
+            'character varying'
+          when col_type ilike 'numeric%' then 'numeric'
+          else col_type
+        end as col_type,
+        case
+          when col_type like 'character%'
+          then nullif(REGEXP_SUBSTR(col_type, '[0-9]+'), '')::int
+          else null
+        end as character_maximum_length,
+        case
+          when col_type like 'numeric%'
+          then nullif(
+            SPLIT_PART(REGEXP_SUBSTR(col_type, '[0-9,]+'), ',', 1),
+            '')::int
+          else null
+        end as numeric_precision,
+        case
+          when col_type like 'numeric%'
+          then nullif(
+            SPLIT_PART(REGEXP_SUBSTR(col_type, '[0-9,]+'), ',', 2),
+            '')::int
+          else null
+        end as numeric_scale
+
+      from pg_get_late_binding_view_cols()
+      cols(view_schema name, view_name name, col_name name,
+           col_type varchar, ordinal_position int)
+      where view_name = '{{ relation.identifier }}'
+    ),
+
+    unioned as (
+      select * from bound_views
+      union all
+      select * from unbound_views
+    )
+
+    select
+      column_name,
+      data_type,
+      character_maximum_length,
+      numeric_precision,
+      numeric_scale
+
+    from unioned
+    {% if relation.schema %}
+    where table_schema = '{{ relation.schema }}'
+    {% endif %}
+    order by ordinal_position
+  {% endcall %}
+  {% set table = load_result('get_columns_in_relation').table %}
+  {{ return(sql_convert_columns_in_relation(table)) }}
+{% endmacro %}
+
+
+{% macro redshift__list_relations_without_caching(database, schema) %}
+  {{ return(postgres__list_relations_without_caching(database, schema)) }}
+{% endmacro %}
+
+
+{% macro redshift__information_schema_name(database) -%}
+  {{ return(postgres__information_schema_name(database)) }}
+{%- endmacro %}
