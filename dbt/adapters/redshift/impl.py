2091c70a50a8cf82dec665ecb881f130fec2088d
Jacob Beck <jake@fishtownanalytics.com>
2019-04-29 19:25:17 -0600

    PR feedback: Make a RedshiftColumn, make the RedshiftAdapter use it

--- dbt/adapters/redshift/impl.py
+++ dbt/adapters/redshift/impl.py
@@ -1,13 +1,12 @@
 from dbt.adapters.postgres import PostgresAdapter
 from dbt.adapters.redshift import RedshiftConnectionManager
-from dbt.adapters.base import Column
+from dbt.adapters.redshift import RedshiftColumn
 from dbt.logger import GLOBAL_LOGGER as logger  # noqa
 
 
 class RedshiftAdapter(PostgresAdapter):
     ConnectionManager = RedshiftConnectionManager
-    # postgres overrides this, so override the override.
-    Column = Column
+    Column = RedshiftColumn
 
     AdapterSpecificConfigs = frozenset({"sort_type", "dist", "sort", "bind"})
 
