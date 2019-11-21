02b4d6fc9997b7bf8697afbc951aaf6e6dc09269
Jacob Beck <jake@fishtownanalytics.com>
2019-11-21 11:13:05 -0700

    just ignore it if we get multiple databases on redshift/postgres, and pick the first one instead

--- dbt/include/redshift/macros/catalog.sql
+++ dbt/include/redshift/macros/catalog.sql
@@ -1,9 +1,6 @@
 
 {% macro redshift__get_base_catalog(information_schemas) -%}
   {%- call statement('base_catalog', fetch_result=True) -%}
-    {% if (information_schemas | length) != 1 %}
-        {{ exceptions.raise_compiler_error('redshift get_catalog requires exactly one database') }}
-    {% endif %}
     {% set database = information_schemas[0].database %}
     {{ adapter.verify_database(database) }}
 
