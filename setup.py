d13edd005c99d690a8ac089512636d5896f8993f
Jacob Beck <jake@fishtownanalytics.com>
2020-05-12 10:45:59 -0600

    Bump version: 0.17.0b1 → 0.17.0rc1

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
 
 
 package_name = "dbt-redshift"
-package_version = "0.17.0b1"
+package_version = "0.17.0rc1"
 description = """The redshift adapter plugin for dbt (data build tool)"""
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
