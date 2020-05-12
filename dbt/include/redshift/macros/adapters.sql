a842dd7e674e8558bebbb593c27b66a6dc861e92
Jacob Beck <jake@fishtownanalytics.com>
2020-05-12 14:24:46 -0600

    Only run alter_column_comment if there are columns defined
    Also remove unused (and confusing/misleading) test data

--- dbt/include/redshift/macros/adapters.sql
+++ dbt/include/redshift/macros/adapters.sql
@@ -192,13 +192,13 @@
 
 
 {% macro redshift__persist_docs(relation, model, for_relation, for_columns) -%}
-  {% if for_relation and config.persist_relation_docs() %}
+  {% if for_relation and config.persist_relation_docs() and model.description %}
     {% do run_query(alter_relation_comment(relation, model.description)) %}
   {% endif %}
 
   {# Override: do not set column comments for LBVs #}
   {% set is_lbv = config.get('materialized') == 'view' and config.get('bind') == false %}
-  {% if for_columns and config.persist_column_docs() and not is_lbv %}
+  {% if for_columns and config.persist_column_docs() and model.columns and not is_lbv %}
     {% do run_query(alter_column_comment(relation, model.columns)) %}
   {% endif %}
 {% endmacro %}
