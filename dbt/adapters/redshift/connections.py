5f26b51cc7e640079396198052e2dddf3e4ba5ae
brunomurino <bfsmurino@gmail.com>
2020-06-22 23:15:40 +0100

    updates to make pep8 compliant

--- dbt/adapters/redshift/connections.py
+++ dbt/adapters/redshift/connections.py
@@ -93,16 +93,16 @@ class RedshiftConnectionManager(PostgresConnectionManager):
 
     @classmethod
     def fetch_cluster_credentials(cls, db_user, db_name, cluster_id,
-                                iam_profile, duration_s, autocreate,
-                                db_groups):
+                                  iam_profile, duration_s, autocreate,
+                                  db_groups):
         """Fetches temporary login credentials from AWS. The specified user
         must already exist in the database, or else an error will occur"""
 
         if iam_profile is None:
             boto_client = boto3.client('redshift')
         else:
-            logger.debug("Connecting to Redshift using 'IAM'"
-                        + f"with profile {iam_profile}")
+            logger.debug("Connecting to Redshift using 'IAM'" +
+                         f"with profile {iam_profile}")
             boto_session = boto3.Session(
                 profile_name=iam_profile,
                 region_name='eu-west-1'
