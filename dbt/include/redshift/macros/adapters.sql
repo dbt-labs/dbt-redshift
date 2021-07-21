47b4ce1b94d2d1cfbae5596c1a537132b8002985
matt-winkler <75497565+matt-winkler@users.noreply.github.com>
2021-07-21 13:49:19 -0600

    Feature: on_schema_change for incremental models (#3387)
    * detect and act on schema changes
    
    * update incremental helpers code
    
    * update changelog
    
    * fix error in diff_columns from testing
    
    * abstract code a bit further
    
    * address matching names vs. data types
    
    * Update CHANGELOG.md
    
    Co-authored-by: Jeremy Cohen <jeremy@fishtownanalytics.com>
    
    * updates from Jeremy's feedback
    
    * multi-column add / remove with full_refresh
    
    * simple changes from JC's feedback
    
    * updated for snowflake
    
    * reorganize postgres code
    
    * reorganize approach
    
    * updated full refresh trigger logic
    
    * fixed unintentional wipe behavior
    
    * catch final else condition
    
    * remove WHERE string replace
    
    * touch ups
    
    * port core to snowflake
    
    * added bigquery code
    
    * updated impacted unit tests
    
    * updates from linting tests
    
    * updates from linting again
    
    * snowflake updates from further testing
    
    * fix logging
    
    * clean up incremental logic
    
    * updated for bigquery
    
    * update postgres with new strategy
    
    * update nodeconfig
    
    * starting integration tests
    
    * integration test for ignore case
    
    * add test for append_new_columns
    
    * add integration test for sync
    
    * remove extra tests
    
    * add unique key and snowflake test
    
    * move incremental integration test dir
    
    * update integration tests
    
    * update integration tests
    
    * Suggestions for #3387 (#3558)
    
    * PR feedback: rationalize macros + logging, fix + expand tests
    
    * Rm alter_column_types, always true for sync_all_columns
    
    * update logging and integration test on sync
    
    * update integration tests
    
    * test fix SF integration tests
    
    Co-authored-by: Matt Winkler <matt.winkler@fishtownanalytics.com>
    
    * rename integration test folder
    
    * Update core/dbt/include/global_project/macros/materializations/incremental/incremental.sql
    
    Accept Jeremy's suggested change
    
    Co-authored-by: Jeremy Cohen <jeremy@fishtownanalytics.com>
    
    * Update changelog [skip ci]
    
    Co-authored-by: Jeremy Cohen <jeremy@fishtownanalytics.com>

--- dbt/include/redshift/macros/adapters.sql
+++ dbt/include/redshift/macros/adapters.sql
@@ -255,3 +255,29 @@
   {% do return(postgres__alter_column_comment(relation, column_dict)) %}
 {% endmacro %}
 
+
+{% macro redshift__alter_relation_add_remove_columns(relation, add_columns, remove_columns) %}
+  
+  {% if add_columns %}
+  
+    {% for column in add_columns %}
+      {% set sql -%}
+          alter {{ relation.type }} {{ relation }} add column {{ column.name }} {{ column.data_type }}
+      {% endset %}
+      {% do run_query(sql) %}
+    {% endfor %}
+
+  {% endif %}
+  
+  {% if remove_columns %}
+  
+    {% for column in remove_columns %}
+      {% set sql -%}
+          alter {{ relation.type }} {{ relation }} drop column {{ column.name }}
+      {% endset %}
+      {% do run_query(sql) %}
+    {% endfor %}
+    
+  {% endif %}
+
+{% endmacro %}
