97e21803efebeaf22b7d45c0732062f6fad37fec
Jeremy Cohen <jeremy@dbtlabs.com>
2021-09-28 16:22:57 +0200

    keep going

--- scripts/build-dist.sh
+++ scripts/build-dist.sh
@@ -14,15 +14,6 @@ rm -rf "$DBT_PATH"/dist
 rm -rf "$DBT_PATH"/build
 mkdir -p "$DBT_PATH"/dist
 
-for SUBPATH in dbt
-do
-    rm -rf "$DBT_PATH"/"$SUBPATH"/dist
-    rm -rf "$DBT_PATH"/"$SUBPATH"/build
-    cd "$DBT_PATH"/"$SUBPATH"
-    $PYTHON_BIN setup.py sdist bdist_wheel
-    cp -r "$DBT_PATH"/"$SUBPATH"/dist/* "$DBT_PATH"/dist/
-done
-
 cd "$DBT_PATH"
 $PYTHON_BIN setup.py sdist bdist_wheel
 
