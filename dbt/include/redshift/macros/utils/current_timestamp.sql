6160a03f8b0bd0ee4af1c0853ad096320980a33c
Doug Beatty <doug.beatty@dbtlabs.com>
2022-05-25 11:01:36 -0600

    Switch namespace from `dbt_utils` to `dbt`

--- dbt/include/redshift/macros/utils/current_timestamp.sql
+++ dbt/include/redshift/macros/utils/current_timestamp.sql
@@ -4,5 +4,5 @@
 
 {# redshift should use default instead of postgres #}
 {% macro redshift__current_timestamp_in_utc() %}
-    {{ return(dbt_utils.default__current_timestamp_in_utc()) }}
+    {{ return(dbt.default__current_timestamp_in_utc()) }}
 {% endmacro %}
--- dbt/include/redshift/macros/utils/last_day.sql
+++ dbt/include/redshift/macros/utils/last_day.sql
@@ -1,6 +1,6 @@
 {# redshift should use default instead of postgres #}
 {% macro redshift__last_day(date, datepart) %}
 
-    {{ return(dbt_utils.default__last_day(date, datepart)) }}
+    {{ return(dbt.default__last_day(date, datepart)) }}
 
 {% endmacro %}
