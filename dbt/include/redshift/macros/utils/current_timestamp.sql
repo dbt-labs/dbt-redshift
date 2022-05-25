5cc28d38c981c4ac0cd809d954b003b9078231d8
Doug Beatty <doug.beatty@dbtlabs.com>
2022-05-25 11:07:28 -0600

    Remove conflicting definition of current_timestamp()

--- dbt/include/redshift/macros/utils/current_timestamp.sql
+++ dbt/include/redshift/macros/utils/current_timestamp.sql
@@ -1,7 +1,3 @@
-{% macro redshift__current_timestamp() %}
-    getdate()
-{% endmacro %}
-
 {# redshift should use default instead of postgres #}
 {% macro redshift__current_timestamp_in_utc() %}
     {{ return(dbt.default__current_timestamp_in_utc()) }}
