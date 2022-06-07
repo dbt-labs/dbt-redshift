bc4328e58d6bcfcda40520440e3fab00491f6e8b
Doug Beatty <doug.beatty@dbtlabs.com>
2022-06-07 14:44:40 -0600

    Specify the namespace explicitly

--- dbt/include/redshift/macros/utils/dateadd.sql
+++ dbt/include/redshift/macros/utils/dateadd.sql
@@ -1,4 +1,4 @@
 {#-- redshift should use default instead of postgres --#}
 {% macro redshift__dateadd(datepart, interval, from_date_or_timestamp) %}
-    {{ return(default__dateadd(datepart, interval, from_date_or_timestamp)) }}
+    {{ return(dbt.default__dateadd(datepart, interval, from_date_or_timestamp)) }}
 {% endmacro %}
