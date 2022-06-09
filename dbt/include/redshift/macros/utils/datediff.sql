0b349a6a0114bb2d6cf5f852964c167f8f57597a
Doug Beatty <doug.beatty@dbtlabs.com>
2022-06-09 15:04:47 -0600

    Duplicate default implementations into Redshift (to avoid inheritance issues)

--- dbt/include/redshift/macros/utils/datediff.sql
+++ dbt/include/redshift/macros/utils/datediff.sql
@@ -1,4 +1,10 @@
 {#-- redshift should use default instead of postgres --#}
 {% macro redshift__datediff(first_date, second_date, datepart) -%}
-    {{ return(dbt.default__datediff(first_date, second_date, datepart)) }}
+
+    datediff(
+        {{ datepart }},
+        {{ first_date }},
+        {{ second_date }}
+        )
+
 {%- endmacro %}
