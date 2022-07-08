af971988b9e24c1f2fe4f9c08d1481396e969638
Doug Beatty <doug.beatty@dbtlabs.com>
2022-07-08 09:54:47 -0600

    Switch to branch with more tests [skip ci]

--- tests/functional/adapter/test_grants.py
+++ tests/functional/adapter/test_grants.py
@@ -1,5 +1,26 @@
-from dbt.tests.adapter.grants.test_models import TestModelGrants
+import pytest
+from dbt.tests.adapter.grants.test_model_grants import BaseModelGrants
+from dbt.tests.adapter.grants.test_incremental_grants import BaseIncrementalGrants
+from dbt.tests.adapter.grants.test_invalid_grants import BaseInvalidGrants
+from dbt.tests.adapter.grants.test_seed_grants import BaseSeedGrants
+from dbt.tests.adapter.grants.test_snapshot_grants import BaseSnapshotGrants
 
 
-class TestModelGrantsRedshift(TestModelGrants):
+class TestModelGrantsRedshift(BaseModelGrants):
+    pass
+
+
+class TestIncrementalGrantsRedshift(BaseIncrementalGrants):
+    pass
+
+
+class TestSeedGrantsRedshift(BaseSeedGrants):
+    pass
+
+
+class TestSnapshotGrantsRedshift(BaseSnapshotGrants):
+    pass
+
+
+class TestInvalidGrantsRedshift(BaseModelGrants):
     pass
