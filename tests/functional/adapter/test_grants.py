eaa680a70e83a6461d14c2dcb7e504db5c5b52b1
Doug Beatty <doug.beatty@dbtlabs.com>
2022-07-11 07:53:51 -0600

    Inherit default tests

--- tests/functional/adapter/test_grants.py
+++ tests/functional/adapter/test_grants.py
@@ -3,18 +3,7 @@ from dbt.tests.adapter.grants.test_model_grants import BaseModelGrants
 from dbt.tests.adapter.grants.test_incremental_grants import BaseIncrementalGrants
 from dbt.tests.adapter.grants.test_invalid_grants import BaseInvalidGrants
 from dbt.tests.adapter.grants.test_seed_grants import BaseSeedGrants
-from dbt.tests.adapter.grants.test_snapshot_grants import BaseSnapshotGrants, snapshot_schema_yml
-
-
-my_snapshot_sql = """
-{% snapshot my_snapshot %}
-    {{ config(
-        check_cols='all', unique_key='id', strategy='check',
-        target_database=database, target_schema=schema
-    ) }}
-    select 1 as id, cast('blue' as {{ type_string() }}) as color
-{% endsnapshot %}
-""".strip()
+from dbt.tests.adapter.grants.test_snapshot_grants import BaseSnapshotGrants
 
 
 class TestModelGrantsRedshift(BaseModelGrants):
@@ -30,9 +19,7 @@ class TestSeedGrantsRedshift(BaseSeedGrants):
 
 
 class TestSnapshotGrantsRedshift(BaseSnapshotGrants):
-    @pytest.fixture(scope="class")
-    def snapshots(self):
-        return {"my_snapshot.sql": my_snapshot_sql, "schema.yml": snapshot_schema_yml}
+    pass
 
 
 class TestInvalidGrantsRedshift(BaseModelGrants):
