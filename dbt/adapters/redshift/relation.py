4480078e0ec03c2ba6f5a4a7aac79a9f9506c3e7
Mila Page <67295367+VersusFacit@users.noreply.github.com>
2022-08-08 11:10:37 -0700

    Ct 964/truncate large suffixes (#149)
    * Copy-paste postgres__make_relation_with_suffix as redshift__make_relation_with_suffix
    
    * Added integration test
    
    * Revert "Added integration test"
    
    This reverts commit d2cff85947f7212fc1fe531d10e497229c54e085.
    
    * Added integration test
    
    * Updated CHANGELOG.md
    
    * Import Goodkat's work and convert/add tests.
    
    Co-authored-by: Goodkat <2552891@gmail.com>
    Co-authored-by: Mila Page <versusfacit@users.noreply.github.com>

--- dbt/adapters/redshift/relation.py
+++ dbt/adapters/redshift/relation.py
@@ -5,8 +5,11 @@ from dbt.adapters.postgres.relation import PostgresRelation
 
 @dataclass(frozen=True, eq=False, repr=False)
 class RedshiftRelation(PostgresRelation):
-    # Override the method in the Postgres Relation
-    # because Redshift allows longer names
+    # Override the method in the Postgres Relation because Redshift allows
+    # longer names: "Be between 1 and 127 bytes in length, not including
+    # quotation marks for delimited identifiers."
+    #
+    # see: https://docs.aws.amazon.com/redshift/latest/dg/r_names.html
     def relation_max_name_length(self):
         return 127
 
