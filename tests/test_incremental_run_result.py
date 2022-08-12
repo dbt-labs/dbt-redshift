1adf08c4472f60b9c59c520eb70cec9e9c66420b
Elize Papineau <elize.papineau@dbtlabs.com>
2022-08-12 15:13:40 -0700

    add test for column precision change

---
+++ tests/test_incremental_run_result.py
@@ -0,0 +1,6 @@
+import pytest
+
+from dbt.tests.adapter.basic.test_incremental import BaseIncrementalNotSchemaChange
+
+class TestBaseIncrementalNotSchemaChange(BaseIncrementalNotSchemaChange):
+    pass
