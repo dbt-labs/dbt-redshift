99a7a058cd9ee619c6decdf4c62c4440756b5897
Dave Connors <dave.connors@fishtownanalytics.com>
2022-12-09 10:53:17 -0600

    do not extend

--- tests/functional/adapter/constraints/test_redshift_constraints.py
+++ tests/functional/adapter/constraints/test_redshift_constraints.py
@@ -5,15 +5,3 @@ from dbt.tests.adapter.constraints.test_constraints import (
     TestModelLevelConstraintsErrorMessages
 )
 
-
-class TestRedshiftModelLevelConstraintsEnabledConfigs(TestModelLevelConstraintsEnabledConfigs):
-    pass
-
-class TestRedshiftModelLevelConstraintsDisabledConfigs(TestModelLevelConstraintsDisabledConfigs):
-    pass
-
-class TestRedshiftSchemaConstraintsEnabledConfigs(TestSchemaConstraintsEnabledConfigs):
-    pass
-
-class TestRedshiftModelLevelConstraintsErrorMessages(TestModelLevelConstraintsErrorMessages):
-    pass
