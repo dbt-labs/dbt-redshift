953b2c1e95b8b05f11951703757c158a0152f4f6
Mike Alfare <mike.alfare@dbtlabs.com>
2023-02-03 19:00:39 -0500

    updated tests based on PR feedback, no functional changes

--- tests/functional/adapter/test_backup_table.py
+++ tests/functional/adapter/test_backup_table.py
@@ -3,28 +3,78 @@ import pytest
 from dbt.tests.util import run_dbt
 
 
-_MODELS = {
-    "backup_is_false.sql": "{{ config(materialized='table', backup=False) }} select 1 as my_col",
-    "backup_is_true.sql": "{{ config(materialized='table', backup=True) }} select 1 as my_col",
-    "backup_is_undefined.sql": "{{ config(materialized='table') }} select 1 as my_col",
-    "backup_is_true_view.sql": "{{ config(materialized='view', backup=True) }} select 1 as my_col",
-    "syntax_with_distkey.sql": "{{ config(materialized='table', backup=False, dist='my_col') }} select 1 as my_col",
-    "syntax_with_sortkey.sql": "{{ config(materialized='table', backup=False, sort='my_col') }} select 1 as my_col",
-}
+_MODEL_BACKUP_IS_FALSE = """
+{{ config(
+    materialized='table',
+    backup=False
+) }}
+select 1 as my_col
+"""
+
+
+_MODEL_BACKUP_IS_TRUE = """
+{{ config(
+    materialized='table',
+    backup=True
+) }}
+select 1 as my_col
+"""
+
+
+_MODEL_IS_UNDEFINED = """
+{{ config(
+    materialized='table'
+) }}
+select 1 as my_col
+"""
+
+
+_MODEL_IS_TRUE_VIEW = """
+{{ config(
+    materialized='view',
+    backup=True
+) }}
+select 1 as my_col
+"""
+
+
+_MODEL_SYNTAX_WITH_DISTKEY = """
+{{ config(
+    materialized='table',
+    backup=False,
+    dist='my_col'
+) }}
+select 1 as my_col
+"""
+
+
+_MODEL_SYNTAX_WITH_SORTKEY = """
+{{ config(
+    materialized='table',
+    backup=False,
+    sort='my_col'
+) }}
+select 1 as my_col
+"""
 
 
 class BackupTableBase:
 
     @pytest.fixture(scope="class", autouse=True)
-    def run_dbt_results(self, project):
+    def _run_dbt(self, project):
         run_dbt(["run"])
+
+
+class TestBackupTableOption(BackupTableBase):
 
     @pytest.fixture(scope="class")
     def models(self):
-        return _MODELS
-
-
-class TestBackupTableModel(BackupTableBase):
+        return {
+            "backup_is_false.sql": _MODEL_BACKUP_IS_FALSE,
+            "backup_is_true.sql": _MODEL_BACKUP_IS_FALSE,
+            "backup_is_undefined.sql": _MODEL_IS_UNDEFINED,
+            "backup_is_true_view.sql": _MODEL_IS_TRUE_VIEW,
+        }
 
     @pytest.mark.parametrize(
         "model_ddl,backup_expected",
@@ -37,8 +87,28 @@ class TestBackupTableModel(BackupTableBase):
         indirect=["model_ddl"]
     )
     def test_setting_reflects_config_option(self, model_ddl: str, backup_expected: bool):
+        """
+        Test different scenarios of configuration at the MODEL level and verify the expected setting for backup
+
+        This test looks for whether `backup no` appears in the DDL file. If it does, then the table will not be backed
+        up. If it does not appear, the table will be backed up.
+
+        Args:
+            model_ddl: the DDL for each model as a string
+            backup_expected: whether backup is expected for this model
+        """
         backup_will_occur = "backup no" not in model_ddl.lower()
         assert backup_will_occur == backup_expected
+
+
+class TestBackupTableSyntax(BackupTableBase):
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "syntax_with_distkey.sql": _MODEL_SYNTAX_WITH_DISTKEY,
+            "syntax_with_sortkey.sql": _MODEL_SYNTAX_WITH_SORTKEY,
+        }
 
     @pytest.mark.parametrize(
         "model_ddl,search_phrase",
@@ -48,15 +118,32 @@ class TestBackupTableModel(BackupTableBase):
         ],
         indirect=["model_ddl"]
     )
-    def test_properly_formed_ddl(self, model_ddl, search_phrase):
+    def test_backup_predicate_precedes_secondary_predicates(self, model_ddl, search_phrase):
+        """
+        Test whether `backup no` appears roughly in the correct spot in the DDL
+
+        This test verifies that the backup predicate comes before the secondary predicates.
+        This test does not guarantee that the resulting DDL is properly formed.
+
+        Args:
+            model_ddl: the DDL for each model as a string
+            search_phrase: the string within the DDL that indicates the distkey or sortkey
+        """
         assert model_ddl.find("backup no") < model_ddl.find(search_phrase)
 
 
-class TestBackupTableProject(BackupTableBase):
+class TestBackupTableProjectDefault(BackupTableBase):
 
     @pytest.fixture(scope="class")
     def project_config_update(self):
         return {"models": {"backup": False}}
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "backup_is_true.sql": _MODEL_BACKUP_IS_FALSE,
+            "backup_is_undefined.sql": _MODEL_IS_UNDEFINED,
+        }
 
     @pytest.mark.parametrize(
         "model_ddl,backup_expected",
@@ -67,5 +154,15 @@ class TestBackupTableProject(BackupTableBase):
         indirect=["model_ddl"]
     )
     def test_setting_defaults_to_project_option(self, model_ddl: str, backup_expected: bool):
+        """
+        Test different scenarios of configuration at the PROJECT level and verify the expected setting for backup
+
+        This test looks for whether `backup no` appears in the DDL file. If it does, then the table will not be backed
+        up. If it does not appear, the table will be backed up.
+
+        Args:
+            model_ddl: the DDL for each model as a string
+            backup_expected: whether backup is expected for this model
+        """
         backup_will_occur = "backup no" not in model_ddl.lower()
         assert backup_will_occur == backup_expected
