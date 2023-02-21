fe4b6429b4c19327630d260c2f57bb2d0953897f
Colin <colin.rogers@dbtlabs.com>
2023-02-21 10:36:22 -0800

    replace tests/postgres branches

--- .github/scripts/update_dev_requirements.sh
+++ .github/scripts/update_dev_requirements.sh
@@ -3,12 +3,18 @@ set -e
 
 git_branch=$1
 target_req_file="dev-requirements.txt"
-req_sed_pattern="s/#egg=dbt-core/@${git_branch}#egg=dbt-core/g"
+core_req_sed_pattern="s/#egg=dbt-core/@${git_branch}#egg=dbt-core/g"
+postgres_req_sed_pattern="s/#egg=dbt-postgres/@${git_branch}#egg=dbt-postgres/g"
+tests_req_sed_pattern="s/#egg=dbt-tests/@${git_branch}#egg=dbt-tests/g"
 if [[ "$OSTYPE" == darwin* ]]; then
  # mac ships with a different version of sed that requires a delimiter arg
- sed -i "" "$req_sed_pattern" $target_req_file
+ sed -i "" "$core_req_sed_pattern" $target_req_file
+ sed -i "" "$postgres_req_sed_pattern" $target_req_file
+ sed -i "" "$tests_req_sed_pattern" $target_req_file
 else
- sed -i "$req_sed_pattern" $target_req_file
+ sed -i "$core_req_sed_pattern" $target_req_file
+ sed -i "$postgres_req_sed_pattern" $target_req_file
+ sed -i "$tests_req_sed_pattern" $target_req_file
 fi
 core_version=$(curl "https://raw.githubusercontent.com/dbt-labs/dbt-core/${git_branch}/core/dbt/version.py" | grep "__version__ = *"|cut -d'=' -f2)
 bumpversion --allow-dirty --new-version "$core_version" major
