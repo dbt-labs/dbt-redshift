d0474f81ce7fca06320820b450383bf877825793
Neelesh Srinivas Salian <neeleshssalian@gmail.com>
2023-02-21 18:47:48 -0800

    autouse

--- tests/functional/adapter/concurrent_transaction/test_concurrent_transaction.py
+++ tests/functional/adapter/concurrent_transaction/test_concurrent_transaction.py
@@ -7,9 +7,7 @@ from tests.functional.adapter.concurrent_transaction.fixtures import *
 
 
 class BaseConcurrentTransaction(SeedConfigBase):
-    query_state = {}
-
-    @pytest.fixture(scope="class")
+    @pytest.fixture(scope="class", autouse=True)
     def setUp(self, project):
         # Resetting the query_state
         self.query_state = {
