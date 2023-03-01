e029cd53fae55875004c3324911d624f03e4e6e0
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-03-01 15:41:16 -0600

    remove dummy value

--- .github/scripts/update_nightly_env.sh
+++ .github/scripts/update_nightly_env.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 set -e
 
-release_branch="1.5.latest"
+release_branch=$1
 target_req_file=".github/workflows/nightly-release.yml"
 if [[ "$OSTYPE" == darwin* ]]; then
  # mac ships with a different version of sed that requires a delimiter arg
