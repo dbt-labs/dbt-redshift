b47da9714b58cc14958bc33d8318dafcb88f2cc5
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-03-02 11:11:03 -0600

    fix regex for update_release_branch

--- .github/scripts/update_release_branch.sh
+++ .github/scripts/update_release_branch.sh
@@ -5,7 +5,7 @@ release_branch=$1
 target_req_file=".github/workflows/nightly-release.yml"
 if [[ "$OSTYPE" == darwin* ]]; then
  # mac ships with a different version of sed that requires a delimiter arg
- sed -i "" "s|(\d+\.)?(\d+\.).latest|${release_branch}" $target_req_file
+ sed -i "" "s|[0-9].[0-9].latest|$release_branch|" $target_req_file
 else
- sed -i "s|(\d+\.)?(\d+\.).latest|${release_branch}" $target_req_file
+ sed -i "s|[0-9].[0-9].latest|$release_branch|" $target_req_file
 fi
