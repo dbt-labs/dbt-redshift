8f922749f8776c0d5888ef184d1eb21f37408ac4
Jessie Chen <jiezhec@amazon.com>
2023-04-07 10:19:37 -0700

    add support for datashared objects and external tables for redshift list_relation_without_caching

--- dbt/include/redshift/macros/adapters.sql
+++ dbt/include/redshift/macros/adapters.sql
@@ -196,12 +196,57 @@
 
     ),
 
+    data_share as (
+      select
+        a.ordinal_position as columnnum,
+        a.schema_name as schemaname,
+        a.column_name as columnname,
+        case
+          when a.data_type ilike 'character varying%' then
+            'character varying'
+          when a.data_type ilike 'numeric%' then 'numeric'
+          else a.data_type
+        end as col_type,
+        case
+          when a.data_type like 'character%'
+          then nullif(REGEXP_SUBSTR(a.character_maximum_length, '[0-9]+'), '')::int
+          else null
+        end as character_maximum_length,
+        case
+          when a.data_type like 'numeric%' or a.data_type ilike 'integer%'
+          then nullif(
+            SPLIT_PART(REGEXP_SUBSTR(a.numeric_precision, '[0-9,]+'), ',', 1),
+            '')::int
+        end as numeric_precision,
+        case
+          when a.data_type like 'numeric%' or a.data_type ilike 'integer%'
+          then nullif(
+            SPLIT_PART(REGEXP_SUBSTR(a.numeric_scale, '[0-9,]+'), ',', 2),
+            '')::int
+          else null
+        end as numeric_scale
+      from svv_all_columns a
+      inner join (
+        select object_name
+        from svv_datashare_objects
+        where share_type = 'INBOUND'
+        and   object_type in ('table', 'view', 'materialized view', 'late binding view')
+        and   object_name = '{{ relation.schema }}' || '.' || '{{ relation.identifier }}'
+      ) b on a.schema_name || '.' || a.table_name  = b.object_name
+      inner join (
+        select consumer_database from SVV_DATASHARES
+        where share_type = 'INBOUND'
+      ) c on c.consumer_database = a.database_name
+    ),
+
     unioned as (
       select * from bound_views
       union all
       select * from unbound_views
       union all
       select * from external_views
+      union all
+      select * from data_share
     )
 
     select
@@ -223,7 +268,36 @@
 
 
 {% macro redshift__list_relations_without_caching(schema_relation) %}
-  {{ return(postgres__list_relations_without_caching(schema_relation)) }}
+  {% call statement('list_relations_without_caching', fetch_result=True) -%}
+    select
+      database_name as database,
+      table_name as name,
+      schema_name as schema,
+      'table' as type
+    from SVV_REDSHIFT_TABLES
+    where schema_name ilike '{{ schema_relation.schema }}'
+    and database_name ilike '{{ schema_relation.database }}'
+    and table_type = 'TABLE'
+    union all
+    select
+      database_name as database,
+      table_name as name,
+      schema_name as schema,
+      'view' as type
+    from SVV_REDSHIFT_TABLES
+    where schema_name ilike '{{ schema_relation.schema }}'
+    and database_name ilike '{{ schema_relation.database }}'
+    and table_type = 'VIEW'
+    union all
+    select
+      '{{ schema_relation.database }}' as database,
+      tablename as name,
+      schemaname as schema,
+      'external_tables' as type
+    from SVV_EXTERNAL_TABLES
+    where schemaname ilike '{{ schema_relation.schema }}'
+  {% endcall %}
+  {{ return(load_result('list_relations_without_caching').table) }}
 {% endmacro %}
 
 
