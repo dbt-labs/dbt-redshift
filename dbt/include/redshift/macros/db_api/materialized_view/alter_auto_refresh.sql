4362fcd5603f6d81d0a518b708a8e47929714251
Mike Alfare <mike.alfare@dbtlabs.com>
2023-04-07 23:22:50 -0400

    updated db_api to use native types and avoid calls to config and relation directly

--- dbt/include/redshift/macros/db_api/materialized_view/alter_auto_refresh.sql
+++ dbt/include/redshift/macros/db_api/materialized_view/alter_auto_refresh.sql
@@ -1,3 +1,6 @@
-{% macro redshift__db_api__materialized_view__alter_auto_refresh(relation, auto_refresh) %}
+{% macro redshift__db_api__materialized_view__alter_auto_refresh(materialized_view_name, auto_refresh) %}
+    {% set %}
+        proxy_view = {{ ref(materialized_view_name) }}
+    {% endset %}
     {{ return(proxy_view) }}
 {% endmacro %}
--- dbt/include/redshift/macros/strategies/materialized_view/update.sql
+++ dbt/include/redshift/macros/strategies/materialized_view/update.sql
@@ -1,8 +1,11 @@
 {% macro redshift__strategy__materialized_view__update(relation, updates, sql=None) %}
     {% if 'sort_key' in updates.keys() or 'dist_key' in updates.keys() %}
-        {{ redshift__strategy__materialized_vew__full_refresh(relation, sql) }}
+        {{ redshift__strategy__materialized_view__full_refresh(relation, sql) }}
     {% elif 'auto_refresh' in updates.keys() %}
-        {% set auto_refresh = updates.get('auto_refresh') %}
-        {{ redshift__db_api__materialized_view__alter_auto_refresh(relation, auto_refresh) }}
+        {% set %}
+            materialied_view_name = str(relation)
+            auto_refresh = updates.get('auto_refresh')
+        {% endset %}
+        {{ redshift__db_api__materialized_view__alter_auto_refresh(materialized_view, auto_refresh) }}
     {% endif %}
 {% endmacro %}
