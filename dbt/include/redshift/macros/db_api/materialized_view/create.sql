d9dc8d943e5cbe96557e8e66a75b560dd77c4a5c
Mike Alfare <mike.alfare@dbtlabs.com>
2023-04-09 13:07:58 -0400

    updated db_api layer to take named arguments instead of a kwargs dict

--- dbt/include/redshift/macros/db_api/materialized_view/create.sql
+++ dbt/include/redshift/macros/db_api/materialized_view/create.sql
@@ -1,4 +1,14 @@
-{% macro redshift__db_api__materialized_view__create(materialized_view_name, sql, kwargs) %}
-    {% set proxy_view = redshift__create_view_as(materialized_view_name, sql) %}
+{% macro redshift__db_api__materialized_view__create(
+    materialized_view_name,
+    sql,
+    backup,
+    auto_refresh,
+    dist_style,
+    dist_key,
+    sort_type,
+    sort_key
+) %}
+    {% set relation = {{ ref(materialized_view_name) }} %}
+    {% set proxy_view = redshift__create_view_as(relation, sql) %}
     {{ return(proxy_view) }}
 {% endmacro %}
--- dbt/include/redshift/macros/strategies/materialized_view/create.sql
+++ dbt/include/redshift/macros/strategies/materialized_view/create.sql
@@ -1,24 +1,26 @@
 {% macro redshift__strategy__materialized_view__create(relation, sql) %}
-    {% set materialized_view_name = str(relation) %}
 
-    {% set kwargs = dict() %}
-        {% if 'backup' in config %}
-            {% set _dummy = kwargs.update({'backup': config.get('backup')}) %}
-        {% endif %}
-        {% if 'auto_refresh' in config %}
-            {% set _dummy = kwargs.update({'auto_refresh': config.get('auto_refresh')}) %}
-        {% endif %}
-        {% if 'dist' in config %}
-            {% set dist = config.get('dist') %}
-            {% if dist in ['all', 'even', 'auto'] %}
-                {% set _dummy = kwargs.update({'dist_style': dist}) %}
-            {% else %}
-                {% set _dummy = kwargs.update({'dist_style': 'key', 'dist_key': dist}) %}
-            {% endif %}
-        {% endif %}
-        {% if 'sort_key' in config %}
-            {% set _dummy = kwargs.update({'sort_type': config.get('sort_type', ''), 'sort_key': config.get('sort')}) %}
-        {% endif %}
+    {% set dist = config.get('dist', '') %}
+    {% if dist == '' %}
+        {% set dist_style = none %}
+        {% set dist_key = none %}
+    {% if dist in ['all', 'even', 'auto'] %}
+        {% set dist_style = dist %}
+        {% set dist_key = none %}
+    {% else %}
+        {% set dist_style = 'key' %}
+        {% set dist_key = dist %}
+    {% endif %}
 
-    {{ redshift__db_api__materialized_view__create(materialized_view, sql, kwargs) }}
+    {{ redshift__db_api__materialized_view__create(
+        materialized_view_name = str(relation),
+        sql = sql,
+        backup = config.get('backup', true),
+        autorefresh = config.get('auto_refresh', false),
+        dist_style = dist_style,
+        dist_key = dist_key,
+        sort_type = config.get('sort_type', none),
+        sort_key = config.get('sort', none)
+    ) }}
+
 {% endmacro %}
