bce3e889b04d886d6423ee2cee69a6e82ccf1cf1
Jessie Chen <jiezhec@amazon.com>
2023-04-14 11:49:05 -0700

    leverage redshift_conn method for list_schemas

--- dbt/include/redshift/macros/adapters.sql
+++ dbt/include/redshift/macros/adapters.sql
@@ -303,7 +303,12 @@
 
 
 {% macro redshift__list_schemas(database) -%}
-  {{ return(postgres__list_schemas(database)) }}
+  {% call statement('list_schemas', fetch_result=True, auto_begin=False) %}
+    select database_name as database
+    from SVV_REDSHIFT_TABLES
+
+  {% endcall %}
+  {{ return(load_result('list_schemas').table) }}
 {%- endmacro %}
 
 
