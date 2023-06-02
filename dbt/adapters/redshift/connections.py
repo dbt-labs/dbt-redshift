b95c562606fb2aac349779b7044c70ea27af605f
Mila Page <67295367+VersusFacit@users.noreply.github.com>
2023-06-02 08:52:55 -0700

    Restore transaction semantics used by dbt-redshift prior to 1.5 (#475) (#476)
    * Rename flag and get autocommit on
    
    * Add changie
    
    * Add test for issue 451
    
    * Add test for issue #454
    
    * Fix spelling.
    
    * Revert names
    
    * update tests
    
    * update the comment to use pep for rationale
    
    ---------
    
    Co-authored-by: Mila Page <versusfacit@users.noreply.github.com>

--- dbt/adapters/redshift/connections.py
+++ dbt/adapters/redshift/connections.py
@@ -77,7 +77,8 @@ class RedshiftCredentials(Credentials):
     sslmode: Optional[str] = None
     retries: int = 1
     region: Optional[str] = None  # if not provided, will be determined from host
-    autocommit: Optional[bool] = False
+    # opt-in by default per team deliberation on https://peps.python.org/pep-0249/#autocommit
+    autocommit: Optional[bool] = True
 
     _ALIASES = {"dbname": "database", "pass": "password"}
 
