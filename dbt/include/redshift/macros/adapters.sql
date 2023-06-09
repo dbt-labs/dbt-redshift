b4c093faf9e25971df2ba78245236bf248f36e29
Mike Alfare <mike.alfare@dbtlabs.com>
2023-06-09 19:48:51 -0400

    type in macro

--- dbt/include/redshift/macros/adapters.sql
+++ dbt/include/redshift/macros/adapters.sql
@@ -335,7 +335,7 @@
 
 {% macro redshift__get_drop_relation_sql(relation) %}
     {%- if relation.is_materialized_view -%}
-        redshift__drop_materialized_view(relation)
+        {{ redshift__drop_materialized_view(relation) }}
     {%- else -%}
         drop {{ relation.type }} if exists {{ relation }} cascade
     {%- endif -%}
