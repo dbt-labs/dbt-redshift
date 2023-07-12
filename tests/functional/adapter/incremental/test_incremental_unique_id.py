9c08b03162bc7819e0669e3609dc2db6908ef51e
Doug Beatty <doug.beatty@dbtlabs.com>
2023-07-12 08:04:16 -0600

    Copy incremental tests from dbt-snowflake into dbt-redshift

--- tests/functional/adapter/incremental/test_incremental_unique_id.py
+++ tests/functional/adapter/incremental/test_incremental_unique_id.py
@@ -1,5 +1,12 @@
+import pytest
 from dbt.tests.adapter.incremental.test_incremental_unique_id import BaseIncrementalUniqueKey
 
 
 class TestUniqueKeyRedshift(BaseIncrementalUniqueKey):
     pass
+
+
+class TestUniqueKeyDeleteInsertRedshift(BaseIncrementalUniqueKey):
+    @pytest.fixture(scope="class")
+    def project_config_update(self):
+        return {"models": {"+incremental_strategy": "delete+insert"}}
