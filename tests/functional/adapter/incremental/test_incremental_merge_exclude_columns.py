314baa895c2bc3d5408ba541614d4229e12a22ca
Doug Beatty <doug.beatty@dbtlabs.com>
2023-07-14 08:19:21 -0600

    Test case when using `merge` with `merge_exclude_columns`

--- tests/functional/adapter/incremental/test_incremental_merge_exclude_columns.py
+++ tests/functional/adapter/incremental/test_incremental_merge_exclude_columns.py
@@ -1,7 +1,17 @@
+import pytest
 from dbt.tests.adapter.incremental.test_incremental_merge_exclude_columns import (
     BaseMergeExcludeColumns,
 )
 
 
+seeds__expected_merge_exclude_columns_csv = """id,msg,color
+1,hello,blue
+2,goodbye,green
+3,NULL,purple
+"""
+
+
 class TestMergeExcludeColumns(BaseMergeExcludeColumns):
-    pass
+    @pytest.fixture(scope="class")
+    def seeds(self):
+        return {"expected_merge_exclude_columns.csv": seeds__expected_merge_exclude_columns_csv}
