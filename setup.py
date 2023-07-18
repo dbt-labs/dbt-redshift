4329f86d510acda8ba604c339cc865a3983f08da
Mike Alfare <mike.alfare@dbtlabs.com>
2023-07-18 15:52:18 -0400

    point `setup.py` to `dbt-core/main` to pick up the alpha, remove these packages from `dev-requirements.txt` since they are the same, remove newly unneeded version functions from `setup.py`

--- setup.py
+++ setup.py
@@ -19,15 +19,12 @@ from pathlib import Path
 from setuptools import setup
 
 
-# pull the long description from the README
+# pull the long description from the README and the version from `__version__.py`
 README = Path(__file__).parent / "README.md"
-
-
-# used for this adapter's version and in determining the compatible dbt-core version
 VERSION = Path(__file__).parent / "dbt/adapters/redshift/__version__.py"
 
 
-def _plugin_version() -> str:
+def version() -> str:
     """
     Pull the package version from the main package version file
     """
@@ -36,43 +33,9 @@ def _plugin_version() -> str:
     return attributes["version"]
 
 
-def _core_patch(plugin_patch: str):
-    """
-    Determines the compatible dbt-core patch given this plugin's patch
-
-    Args:
-        plugin_patch: the version patch of this plugin
-    """
-    pre_release_phase = "".join([i for i in plugin_patch if not i.isdigit()])
-    if pre_release_phase:
-        if pre_release_phase not in ["a", "b", "rc"]:
-            raise ValueError(f"Invalid prerelease patch: {plugin_patch}")
-        return f"0{pre_release_phase}1"
-    return "0"
-
-
-# require a compatible minor version (~=) and prerelease if this is a prerelease
-def _core_version(plugin_version: str = _plugin_version()) -> str:
-    """
-    Determine the compatible dbt-core version give this plugin's version.
-
-    We assume that the plugin must agree with `dbt-core` down to the minor version.
-
-    Args:
-        plugin_version: the version of this plugin, this is an argument in case we ever want to unit test this
-    """
-    try:
-        # *_ may indicate a dev release which won't affect the core version needed
-        major, minor, plugin_patch, *_ = plugin_version.split(".", maxsplit=3)
-    except ValueError:
-        raise ValueError(f"Invalid version: {plugin_version}")
-
-    return f"{major}.{minor}.{_core_patch(plugin_patch)}"
-
-
 setup(
     name="dbt-redshift",
-    version=_plugin_version(),
+    version=version(),
     description="The Redshift adapter plugin for dbt",
     long_description=README.read_text(),
     long_description_content_type="text/markdown",
@@ -82,10 +45,11 @@ setup(
     packages=find_namespace_packages(include=["dbt", "dbt.*"]),
     include_package_data=True,
     install_requires=[
-        f"dbt-core~={_core_version()}",
-        f"dbt-postgres~={_core_version()}",
+        "dbt-core @ git+https://github.com/dbt-labs/dbt-core.git#egg=dbt-core&subdirectory=core",
+        "dbt-postgres @ git+https://github.com/dbt-labs/dbt-core.git#egg=dbt-postgres&subdirectory=plugins/postgres",
         "boto3~=1.26.157",
-        # dbt-redshift depends deeply on this package. it does not follow SemVer, therefore there have been breaking changes in previous patch releases
+        # dbt-redshift depends deeply on this package. it does not follow SemVer,
+        # therefore there have been breaking changes in previous patch releases
         # Pin to the patch or minor version, and bump in each new minor version of dbt-redshift.
         "redshift-connector==2.0.913",
         # installed via dbt-core but referenced directly; don't pin to avoid version conflicts with dbt-core
