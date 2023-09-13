b1d97e917189c9be1be11e86e9fec247177b7a0f
Colin <colin.rogers@dbtlabs.com>
2023-09-13 10:42:19 -0700

    PR fixes

--- tests/functional/adapter/materialized_view_tests/test_materialized_views.py
+++ tests/functional/adapter/materialized_view_tests/test_materialized_views.py
@@ -70,12 +70,6 @@ class TestRedshiftMaterializedViewsBasic(MaterializedViewBasic):
         )
         assert self.query_relation_type(project, my_materialized_view) == "materialized_view"
 
-    def test_table_replaces_materialized_view(self, project, my_materialized_view):
-        super().test_table_replaces_materialized_view(project, my_materialized_view)
-
-    def test_view_replaces_materialized_view(self, project, my_materialized_view):
-        super().test_view_replaces_materialized_view(project, my_materialized_view)
-
 
 class RedshiftMaterializedViewChanges(MaterializedViewChanges):
     @pytest.fixture(scope="class", autouse=True)
