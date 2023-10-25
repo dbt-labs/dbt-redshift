0aa8fb1c754c22ebe031e81469e4c0ccce45dad4
Soksamnang Lim <soklim@amazon.com>
2023-10-25 13:21:45 -0700

    Split model grants tests into view and table tests

--- tests/functional/adapter/grants/test_model_table_grants.py
+++ tests/functional/adapter/grants/test_model_table_grants.py
@@ -10,28 +10,10 @@ my_model_sql = """
   select 1 as fun
 """
 
-model_schema_yml = """
-version: 2
-models:
-  - name: my_model
-    config:
-      grants:
-        select: ["{{ env_var('DBT_TEST_USER_1') }}"]
-"""
-
-user2_model_schema_yml = """
-version: 2
-models:
-  - name: my_model
-    config:
-      grants:
-        select: ["{{ env_var('DBT_TEST_USER_2') }}"]
-"""
-
 table_model_schema_yml = """
 version: 2
 models:
-  - name: my_model
+  - name: my_model_table
     config:
       materialized: table
       grants:
@@ -41,7 +23,7 @@ models:
 user2_table_model_schema_yml = """
 version: 2
 models:
-  - name: my_model
+  - name: my_model_table
     config:
       materialized: table
       grants:
@@ -51,7 +33,7 @@ models:
 multiple_users_table_model_schema_yml = """
 version: 2
 models:
-  - name: my_model
+  - name: my_model_table
     config:
       materialized: table
       grants:
@@ -61,7 +43,7 @@ models:
 multiple_privileges_table_model_schema_yml = """
 version: 2
 models:
-  - name: my_model
+  - name: my_model_table
     config:
       materialized: table
       grants:
@@ -73,7 +55,7 @@ models:
 extended_table_model_schema_yml = """
 version: 2
 models:
-  - name: my_model
+  - name: my_model_table
     config:
       materialized: table
       grants:
@@ -87,7 +69,7 @@ models:
 extended2_table_model_schema_yml = """
 version: 2
 models:
-  - name: my_model
+  - name: my_model_table
     config:
       materialized: table
       grants:
@@ -101,7 +83,7 @@ models:
 extended_multiple_grantees_table_model_schema_yml = """
 version: 2
 models:
-  - name: my_model
+  - name: my_model_table
     config:
       materialized: table
       grants:
@@ -114,7 +96,7 @@ models:
 extended_multiple_privileges_table_model_schema_yml = """
 version: 2
 models:
-  - name: my_model
+  - name: my_model_table
     config:
       materialized: table
       grants:
@@ -132,13 +114,13 @@ models:
 class TestModelGrantsRedshift(BaseGrantsRedshift):
     @pytest.fixture(scope="class")
     def models(self):
-        updated_schema = self.interpolate_name_overrides(model_schema_yml)
+        updated_schema = self.interpolate_name_overrides(table_model_schema_yml)
         return {
-            "my_model.sql": my_model_sql,
+            "my_model_table.sql": my_model_sql,
             "schema.yml": updated_schema,
         }
 
-    def test_view_table_grants(self, project, get_test_users, get_test_groups, get_test_roles):
+    def test_table_grants(self, project, get_test_users, get_test_groups, get_test_roles):
         # Override/refactor the tests from dbt-core #
         # we want the test to fail, not silently skip
         test_users = get_test_users
@@ -150,40 +132,15 @@ class TestModelGrantsRedshift(BaseGrantsRedshift):
         assert len(test_groups) == 3
         assert len(test_roles) == 3
 
-        # View materialization, single select grant
-        results = run_dbt(["run"])
-        assert len(results) == 1
-        manifest = get_manifest(project.project_root)
-        model_id = "model.test.my_model"
-        model = manifest.nodes[model_id]
-        # user configuration for grants
-        user_expected = {select_privilege_name: [test_users[0]]}
-        assert model.config.grants == user_expected
-        assert model.config.materialized == "view"
-        # new configuration for grants
-        expected = {select_privilege_name: {"user": [test_users[0]]}}
-        self.assert_expected_grants_match_actual(project, "my_model", expected)
-
-        # View materialization, change select grant user
-        updated_yaml = self.interpolate_name_overrides(user2_model_schema_yml)
-        write_file(updated_yaml, project.project_root, "models", "schema.yml")
-        results = run_dbt(["run"])
-        assert len(results) == 1
-
-        expected = {select_privilege_name: {"user": [test_users[1]]}}
-        self.assert_expected_grants_match_actual(project, "my_model", expected)
-
         # Table materialization, single select grant
-        updated_yaml = self.interpolate_name_overrides(table_model_schema_yml)
-        write_file(updated_yaml, project.project_root, "models", "schema.yml")
         results = run_dbt(["run"])
         assert len(results) == 1
         manifest = get_manifest(project.project_root)
-        model_id = "model.test.my_model"
+        model_id = "model.test.my_model_table"
         model = manifest.nodes[model_id]
         assert model.config.materialized == "table"
         expected = {select_privilege_name: {"user": [test_users[0]]}}
-        self.assert_expected_grants_match_actual(project, "my_model", expected)
+        self.assert_expected_grants_match_actual(project, "my_model_table", expected)
 
         # Table materialization, change select grant user
         updated_yaml = self.interpolate_name_overrides(user2_table_model_schema_yml)
@@ -194,7 +151,7 @@ class TestModelGrantsRedshift(BaseGrantsRedshift):
         model = manifest.nodes[model_id]
         assert model.config.materialized == "table"
         expected = {select_privilege_name: {"user": [test_users[1]]}}
-        self.assert_expected_grants_match_actual(project, "my_model", expected)
+        self.assert_expected_grants_match_actual(project, "my_model_table", expected)
 
         # Table materialization, multiple grantees
         updated_yaml = self.interpolate_name_overrides(multiple_users_table_model_schema_yml)
@@ -205,7 +162,7 @@ class TestModelGrantsRedshift(BaseGrantsRedshift):
         model = manifest.nodes[model_id]
         assert model.config.materialized == "table"
         expected = {select_privilege_name: {"user": [test_users[0], test_users[1]]}}
-        self.assert_expected_grants_match_actual(project, "my_model", expected)
+        self.assert_expected_grants_match_actual(project, "my_model_table", expected)
 
         # Table materialization, multiple privileges
         updated_yaml = self.interpolate_name_overrides(multiple_privileges_table_model_schema_yml)
@@ -224,7 +181,7 @@ class TestModelGrantsRedshift(BaseGrantsRedshift):
             select_privilege_name: {"user": [test_users[0]]},
             insert_privilege_name: {"user": [test_users[1]]},
         }
-        self.assert_expected_grants_match_actual(project, "my_model", expected)
+        self.assert_expected_grants_match_actual(project, "my_model_table", expected)
 
         # Additional tests for privilege grants to extended permission types
         # Table materialization, single select grant
@@ -242,8 +199,8 @@ class TestModelGrantsRedshift(BaseGrantsRedshift):
                 "role": [test_roles[0]],
             }
         }
-        breakpoint()
-        self.assert_expected_grants_match_actual(project, "my_model", expected)
+        # breakpoint()
+        self.assert_expected_grants_match_actual(project, "my_model_table", expected)
 
         # Table materialization, change select grant
         updated_yaml = self.interpolate_name_overrides(extended2_table_model_schema_yml)
@@ -260,7 +217,7 @@ class TestModelGrantsRedshift(BaseGrantsRedshift):
                 "role": [test_roles[1]],
             }
         }
-        self.assert_expected_grants_match_actual(project, "my_model", expected)
+        self.assert_expected_grants_match_actual(project, "my_model_table", expected)
 
         # Table materialization, multiple grantees
         updated_yaml = self.interpolate_name_overrides(
@@ -279,7 +236,7 @@ class TestModelGrantsRedshift(BaseGrantsRedshift):
                 "role": [test_roles[0], test_roles[1]],
             }
         }
-        self.assert_expected_grants_match_actual(project, "my_model", expected)
+        self.assert_expected_grants_match_actual(project, "my_model_table", expected)
 
         # Table materialization, multiple privileges
         updated_yaml = self.interpolate_name_overrides(
@@ -303,4 +260,4 @@ class TestModelGrantsRedshift(BaseGrantsRedshift):
                 "role": [test_roles[1]],
             },
         }
-        self.assert_expected_grants_match_actual(project, "my_model", expected)
+        self.assert_expected_grants_match_actual(project, "my_model_table", expected)
---
+++ tests/functional/adapter/grants/test_model_view_grants.py
@@ -0,0 +1,80 @@
+from base_grants import BaseGrantsRedshift
+import pytest
+from dbt.tests.util import (
+    run_dbt,
+    get_manifest,
+    write_file,
+)
+
+my_model_sql = """
+  select 1 as fun
+"""
+
+model_schema_yml = """
+version: 2
+models:
+  - name: my_model_view
+    config:
+      materialized: view
+      grants:
+        select: ["{{ env_var('DBT_TEST_USER_1') }}"]
+"""
+
+user2_model_schema_yml = """
+version: 2
+models:
+  - name: my_model_view
+    config:
+      materialized: view
+      grants:
+        select: ["{{ env_var('DBT_TEST_USER_2') }}"]
+"""
+
+
+# class BaseModelGrants(BaseGrantsRedshift):
+
+class TestModelGrantsView(BaseGrantsRedshift):
+    @pytest.fixture(scope="class")
+    def models(self):
+        updated_schema = self.interpolate_name_overrides(model_schema_yml)
+        return {
+            "my_model_view.sql": my_model_sql,
+            "schema.yml": updated_schema,
+        }
+
+    def test_view_table_grants(self, project, get_test_users, get_test_groups, get_test_roles):
+        # Override/refactor the tests from dbt-core #
+        # we want the test to fail, not silently skip
+        test_users = get_test_users
+        test_groups = get_test_groups
+        test_roles = get_test_roles
+        select_privilege_name = self.privilege_grantee_name_overrides()["select"]
+        assert len(test_users) == 3
+        assert len(test_groups) == 3
+        assert len(test_roles) == 3
+
+        # View materialization, single select grant
+        updated_yaml = self.interpolate_name_overrides(model_schema_yml)
+        write_file(updated_yaml, project.project_root, "models", "schema.yml")
+        results = run_dbt(["run"])
+        assert len(results) == 1
+
+        manifest = get_manifest(project.project_root)
+        model_id = "model.test.my_model_view"
+        model = manifest.nodes[model_id]
+        # user configuration for grants
+        user_expected = {select_privilege_name: [test_users[0]]}
+        assert model.config.grants == user_expected
+        assert model.config.materialized == "view"
+        # new configuration for grants
+        expected = {select_privilege_name: {"user": [test_users[0]]}}
+        self.assert_expected_grants_match_actual(project, "my_model_view", expected)
+
+        # View materialization, change select grant user
+        updated_yaml = self.interpolate_name_overrides(user2_model_schema_yml)
+        write_file(updated_yaml, project.project_root, "models", "schema.yml")
+        results = run_dbt(["run"])
+        assert len(results) == 1
+
+        expected = {select_privilege_name: {"user": [test_users[1]]}}
+        self.assert_expected_grants_match_actual(project, "my_model_view", expected)
