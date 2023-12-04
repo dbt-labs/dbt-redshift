63c650c9ece4ee53caef50f5b41ec063b6bff5b5
Michelle Ark <michelle.ark@dbtlabs.com>
2023-12-04 16:12:29 +0900

    first pass

---
+++ tests/functional/adapter/unit_testing/test_unit_testing_types.py
@@ -0,0 +1,31 @@
+import pytest
+from dbt.tests.adapter.unit_testing.test_unit_testing_types import BaseUnitTestingTypes
+
+
+class TestRedshiftUnitTestingTypes(BaseUnitTestingTypes):
+    @pytest.fixture
+    def data_types(self):
+        # sql_value, yaml_value
+        return [
+            ["1", "1"],
+            ["1.0", "1.0"],
+            ["'1'", "1"],
+            ["'1'::numeric", "1"],
+            ["'string'", "string"],
+            ["true", "true"],
+            ["DATE '2020-01-02'", "2020-01-02"],
+            ["TIMESTAMP '2013-11-03 00:00:00-0'", "2013-11-03 00:00:00-0"],
+            ["TIMESTAMPTZ '2013-11-03 00:00:00-0'", "2013-11-03 00:00:00-0"],
+            ["ARRAY[1,2,3]", """'{1, 2, 3}'"""],
+            ["ARRAY[1.0,2.0,3.0]", """'{1.0, 2.0, 3.0}'"""],
+            ["ARRAY[1::numeric,2::numeric,3::numeric]", """'{1.0, 2.0, 3.0}'"""],
+            ["ARRAY['a','b','c']", """'{"a", "b", "c"}'"""],
+            ["ARRAY[true,true,false]", """'{true, true, false}'"""],
+            ["ARRAY[DATE '2020-01-02']", """'{"2020-01-02"}'"""],
+            ["ARRAY[TIMESTAMP '2013-11-03 00:00:00-0']", """'{"2013-11-03 00:00:00-0"}'"""],
+            ["ARRAY[TIMESTAMPTZ '2013-11-03 00:00:00-0']", """'{"2013-11-03 00:00:00-0"}'"""],
+            [
+                """'{"bar": "baz", "balance": 7.77, "active": false}'::json""",
+                """'{"bar": "baz", "balance": 7.77, "active": false}'""",
+            ],
+        ]
