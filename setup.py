8488c64bc93c72e2efda1d35ee07ce53eaf5bd24
Colin <colin.rogers@dbtlabs.com>
2024-01-25 13:57:24 -0800

    fix postgres dep

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     install_requires=[
         "dbt-common<1.0",
         "dbt-adapters~=0.1.0a1",
-        "dbt-postgres>=1.8,<1.9",
+        f"dbt-postgres~={_plugin_version()}",
         # dbt-redshift depends deeply on this package. it does not follow SemVer, therefore there have been breaking changes in previous patch releases
         # Pin to the patch or minor version, and bump in each new minor version of dbt-redshift.
         "redshift-connector<=2.0.918, >=2.0.913",
