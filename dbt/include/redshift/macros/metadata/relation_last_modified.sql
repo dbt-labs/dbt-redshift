a711c927b6c9809cf92868d854227ab18ea08531
Mike Alfare <mike.alfare@dbtlabs.com>
2024-02-20 23:13:06 -0500

    add metadata-based source freshness query

--- dbt/include/redshift/macros/metadata/relation_last_modified.sql
+++ dbt/include/redshift/macros/metadata/relation_last_modified.sql
@@ -1,20 +1,27 @@
 {% macro redshift__get_relation_last_modified(information_schema, relations) -%}
 
     {%- call statement('last_modified', fetch_result=True) -%}
-        select table_schema as schema,
-               table_name as identifier,
-               null as last_modified,  -- svv_tables does not have this, research where to get this information
-               {{ current_timestamp() }} as snapshotted_at
-        from svv_tables
-        where (
+        select
+            ns.nspname as "schema",
+            c.relname as identifier,
+            max(qd.start_time) as last_modified,
+            {{ current_timestamp() }} as snapshotted_at
+        from pg_class c
+        join pg_namespace ns
+            on ns.oid = c.relnamespace
+        join sys_query_detail qd
+            on qd.table_id = c.oid
+        where qd.step_name in ('insert', 'merge', 'mergejoin')
+        and (
             {%- for relation in relations -%}
                 (
-                    upper(table_schema) = upper('{{ relation.schema }}')
-                and upper(table_name) = upper('{{ relation.identifier }}')
+                    upper(ns.nspname) = upper('{{ relation.schema }}')
+                and upper(c.relname) = upper('{{ relation.identifier }}')
                 )
                 {%- if not loop.last %} or {% endif -%}
             {%- endfor -%}
         )
+        group by 1, 2, 4
     {%- endcall -%}
 
     {{ return(load_result('last_modified')) }}
