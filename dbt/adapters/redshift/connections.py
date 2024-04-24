b3c1c1af9223e24bbb13290d419e0aa044e9c399
Mike Alfare <mike.alfare@dbtlabs.com>
2024-04-24 15:21:33 -0400

    move iam user specific config out of iam and into iam user

--- dbt/adapters/redshift/connections.py
+++ dbt/adapters/redshift/connections.py
@@ -209,6 +209,18 @@ class RedshiftConnectMethodFactory:
         kwargs = self._iam_kwargs
         kwargs.update(user="", password="")
 
+        if self.credentials.access_key_id and self.credentials.secret_access_key:
+            kwargs.update(
+                access_key_id=self.credentials.access_key_id,
+                secret_access_key=self.credentials.secret_access_key,
+            )
+        elif self.credentials.access_key_id or self.credentials.secret_access_key:
+            raise FailedToConnectError(
+                "'access_key_id' and 'secret_access_key' are both needed if providing explicit credentials"
+            )
+        else:
+            kwargs.update(profile=self.credentials.iam_profile)
+
         if user := self.credentials.user:
             kwargs.update(db_user=user)
         else:
@@ -231,18 +243,6 @@ class RedshiftConnectMethodFactory:
                 "    'cluster_id' must be provided for provisioned cluster"
                 "    'host' must be provided for serverless endpoint"
             )
-
-        if self.credentials.access_key_id and self.credentials.secret_access_key:
-            kwargs.update(
-                access_key_id=self.credentials.access_key_id,
-                secret_access_key=self.credentials.secret_access_key,
-            )
-        elif self.credentials.access_key_id or self.credentials.secret_access_key:
-            raise FailedToConnectError(
-                "'access_key_id' and 'secret_access_key' are both needed if providing explicit credentials"
-            )
-        else:
-            kwargs.update(profile=self.credentials.iam_profile)
 
         return kwargs
 
