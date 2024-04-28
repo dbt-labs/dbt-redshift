0145ba5d6b87b88ed1823bfca173a1d8f1d8f98d
Mike Alfare <mike.alfare@dbtlabs.com>
2024-04-28 12:16:46 -0400

    simplify test script

--- docker/test.sh
+++ docker/test.sh
@@ -1,22 +1,19 @@
 # - VERY rudimentary test script to run latest + specific branch image builds and test them all by running `--version`
 # TODO: create a real test suite
+set -e
 
-clear \
-&& echo "\n\n"\
-"########################################\n"\
-"##### Testing dbt-redshift latest #####\n"\
-"########################################\n"\
-&& docker build --tag dbt-redshift \
-  --target dbt-redshift \
-  docker \
-&& docker run dbt-redshift --version \
-\
-&& echo "\n\n"\
-"#########################################\n"\
-"##### Testing dbt-redshift-1.0.0b1 #####\n"\
-"#########################################\n"\
-&& docker build --tag dbt-redshift-1.0.0b1 \
-  --target dbt-redshift \
-  --build-arg commit_ref=v1.0.0b1 \
-  docker \
-&& docker run dbt-redshift-1.0.0b1 --version
+echo "\n\n"
+echo "#######################################\n"
+echo "##### Testing dbt-redshift latest #####\n"
+echo "#######################################\n"
+
+docker build --tag dbt-redshift --target dbt-redshift docker
+docker run dbt-redshift --version
+
+echo "\n\n"
+echo "########################################\n"
+echo "##### Testing dbt-redshift-1.0.0b1 #####\n"
+echo "########################################\n"
+
+docker build --tag dbt-redshift-1.0.0b1 --target dbt-redshift --build-arg commit_ref=v1.0.0b1 docker
+docker run dbt-redshift-1.0.0b1 --version
