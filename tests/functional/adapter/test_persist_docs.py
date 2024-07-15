91f56bdb8e9a25673ea2b64e2ad735903bc434cd
Jeremy Yeo <jeremyyeo@gmail.com>
2024-07-15 14:37:00 +1200

    add test and changie

--- tests/functional/adapter/test_persist_docs.py
+++ tests/functional/adapter/test_persist_docs.py
@@ -1,6 +1,7 @@
 import json
 import pytest
 
+from dbt.tests.adapter.materialized_view import files
 from dbt.tests.util import run_dbt
 
 from dbt.tests.adapter.persist_docs.test_persist_docs import (
@@ -9,6 +10,21 @@ from dbt.tests.adapter.persist_docs.test_persist_docs import (
     BasePersistDocsColumnMissing,
     BasePersistDocsCommentOnQuotedColumn,
 )
+
+_MATERIALIZED_VIEW_PROPERTIES__SCHEMA_YML = """
+version: 2
+models:
+  - name: my_materialized_view
+    description: |
+      Materialized view model description "with double quotes"
+      and with 'single  quotes' as welll as other;
+      '''abc123'''
+      reserved -- characters
+      80% of statistics are made up on the spot
+      --
+      /* comment */
+      Some $lbl$ labeled $lbl$ and $$ unlabeled $$ dollar-quoting
+"""
 
 
 class TestPersistDocs(BasePersistDocs):
@@ -62,3 +78,30 @@ class TestPersistDocsLateBinding(BasePersistDocsBase):
 
         no_docs_node = catalog_data["nodes"]["model.test.no_docs_model"]
         self._assert_has_view_comments(no_docs_node, False, False)
+
+
+class TestPersistDocsWithMaterializedView(BasePersistDocs):
+    @pytest.fixture(scope="class", autouse=True)
+    def seeds(self):
+        return {"my_seed.csv": files.MY_SEED}
+
+    @pytest.fixture(scope="class")
+    def models(self):
+        return {
+            "my_materialized_view.sql": files.MY_MATERIALIZED_VIEW,
+        }
+
+    @pytest.fixture(scope="class")
+    def properties(self):
+        return {
+            "schema.yml": _MATERIALIZED_VIEW_PROPERTIES__SCHEMA_YML,
+        }
+
+    def test_has_comments_redshift_materialized_view(self, project):
+        run_dbt(["docs", "generate"])
+        with open("target/catalog.json") as fp:
+            catalog_data = json.load(fp)
+        assert "nodes" in catalog_data
+        assert len(catalog_data["nodes"]) == 2
+        view_node = catalog_data["nodes"]["model.test.my_materialized_view"]
+        assert view_node["metadata"]["comment"].startswith("Materialized view model description")
